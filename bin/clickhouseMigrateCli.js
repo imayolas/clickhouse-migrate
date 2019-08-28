fabb096aec1cbefb91fc26ec7e2abe61ff49f01c
Isaac <imayolas@gmail.com>
2019-08-28 13:57:52 +0200

    interim cmmit

--- bin/clickhouseMigrateCli.js
+++ bin/clickhouseMigrateCli.js
@@ -1,11 +1,34 @@
 #!/usr/bin/env node
-const { spawn } = require("child_process")
 const Promise = require("bluebird")
-// const spawnAsync =
-//     spawn(
-//       "knex",
-//       ["migrate:make", "--cwd", "migrations", "--migrations-directory", "warehouse", migrationName],
-//       { stdio: [process.stdin, process.stdout, process.stderr] },
-//       cb
-//     )
+const minimist = require("minimist")
+const { spawn } = require("child_process")
+const argv = minimist(process.argv.slice(2))
 
+const spawnAsync = (command, params = []) => {
+  return new Promise((resolve, reject) => {
+    const spawned = spawn(command, params, { stdio: [process.stdin, process.stdout, process.stderr] })
+    spawned.on("close", () => {
+      resolve()
+    })
+    spawned.on("error", (error) => {
+      reject(error)
+    })
+  })
+}
+
+const run = (command, subCommand) => {
+
+  if(command === "migrate:make") {
+    return spawnAsync("npx", ["migrate", "create", subCommand])
+  }
+
+  if(command === "migrate:latest") {
+    return spawnAsync("npx", ["migrate", "latest", '--store=./lib/stateStorage.js'])
+  }
+
+}
+
+const command = argv._[0]
+const subCommand = argv._[1]
+
+run(command, subCommand)
